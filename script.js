70e5f800d4abdf7638e35a566f0eff3688f1428a
valeeei <167794768+valeeei@users.noreply.github.com>
2024-05-12 13:16:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+.calculator {
+  width: 200px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.screen {
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 5px;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+}
+
+button {
+  padding: 10px;
+  margin: 5px;
+  font-size: 20px;
+  cursor: pointer;
+}
+main {
+    margin: 10px;
+    padding: 5px 7px 5px;
+}
+
+input {
+    text-align: right;
+    background-color: white;
+    width: 330px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    font-size: 30px;
+}
+
+table {
+    display: inline-block;
+    padding: 20px;
+    background-color: grey;
+    border-radius: 10px;
+}
+
+.displaytable {
+    display: table;
+}
+
+.displaytable-cell {
+    display: table-cell;
+}
+  
+.button {
+    width: 60px;
+    height: 35px;
+    font-size: 20px;
+    border-radius: 5px;
+}
+
+.blackbutton { 
+    background-color: black; 
+    color: white;
+}
+
+.greenbutton {
+    background-color: #91ff7d;
+}
+
+.greybutton {
+    background-color: #cccccc;
+}
+
+.bluebutton {
+    background-color: #6288f3;
+}
+
+.redbutton {
+    background-color: #f76767;
+}
